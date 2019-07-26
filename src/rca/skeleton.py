55bcb3f20f6d2e55f02b877c06ba509fac0c9f7b
alexishunzinger <44713903+alexishunzinger@users.noreply.github.com>
2019-07-26 11:39:28 -0700

    Made more user-friendly by combining and condensing functions and scripts (#3)
    * Forcing commit error
    
    * Some functions for PPI and RHI reduced and combined into a single .py file
    
    Forcing commit error
    
    * Ran Black on all .py files
    
    * Test space
    
    * Scripts modified and condensed for use with *any* radar and use for both PPI and RHI
    
    * JSON config files added for C, X, and Ka-band PPI and RHI options. ONe script modified to use JSON file.
    
    * Adjustments to JSON files and consolidating clutter map script
    
    * JSON config files completed and all functions ready and added documentation
    
    * New files updated using Black formatting
    
    * Removed duplicate files or outdated scripts
    
    * Paths included in JSON files and docstrings included in scripts and functions

--- src/rca/skeleton.py
+++ src/rca/skeleton.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+
 This is a skeleton file that can serve as a starting point for a Python
 console script. To run this script uncomment the following line in the
 entry_points section in setup.py:
@@ -41,8 +42,8 @@ def fib(n):
     """
     assert n > 0
     a, b = 1, 1
-    for i in range(n-1):
-        a, b = b, a+b
+    for i in range(n - 1):
+        a, b = b, a + b
     return a
 
 
@@ -55,31 +56,27 @@ def parse_args(args):
     Returns:
       :obj:`argparse.Namespace`: command line parameters namespace
     """
-    parser = argparse.ArgumentParser(
-        description="Just a Fibonnaci demonstration")
+    parser = argparse.ArgumentParser(description="Just a Fibonnaci demonstration")
     parser.add_argument(
-        '--version',
-        action='version',
-        version='rca {ver}'.format(ver=__version__))
+        "--version", action="version", version="rca {ver}".format(ver=__version__)
+    )
+    parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
     parser.add_argument(
-        dest="n",
-        help="n-th Fibonacci number",
-        type=int,
-        metavar="INT")
-    parser.add_argument(
-        '-v',
-        '--verbose',
+        "-v",
+        "--verbose",
         dest="loglevel",
         help="set loglevel to INFO",
-        action='store_const',
-        const=logging.INFO)
+        action="store_const",
+        const=logging.INFO,
+    )
     parser.add_argument(
-        '-vv',
-        '--very-verbose',
+        "-vv",
+        "--very-verbose",
         dest="loglevel",
         help="set loglevel to DEBUG",
-        action='store_const',
-        const=logging.DEBUG)
+        action="store_const",
+        const=logging.DEBUG,
+    )
     return parser.parse_args(args)
 
 
@@ -90,8 +87,9 @@ def setup_logging(loglevel):
       loglevel (int): minimum loglevel for emitting messages
     """
     logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(level=loglevel, stream=sys.stdout,
-                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
 
 
 def main(args):
