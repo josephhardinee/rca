acfad9b3dfa3e9b8963d61d3358bd58f9268ced9
Alexis Hunzinger <a.e.hunzinger@gmail.com>
2019-08-23 09:48:18 -0700

    Experimenting with testing

--- tests/alexis_test.py
+++ tests/alexis_test.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from rca.src.rca.module.calculate_dbz95 import calculate_dbz95_ppi
+#from rca.src.rca.module.calculate_dbz95 import calculate_dbz95_ppi
 
 __author__ = "Alexis Hunzinger"
 __copyright__ = "Alexis Hunzinger"
@@ -14,11 +14,9 @@ def a_name(x):
     if x[0] == 'A':
         return(1)
     else:
-	return(0)
+        return(0)
 
 def test_a_name():
     assert a_name('Alexis') == 1
     assert a_name('Brian') == 0
     assert a_name('Apple') == 1
-    with pytest.raises(AssertionError):
-        a_name('Zenith') == 1
